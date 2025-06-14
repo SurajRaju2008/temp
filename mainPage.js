b1a39cd7f8a547539d7c698bb45c968c90b35825
SurajRaju2008 <surajraju@icloud.com>
2025-06-14 12:51:13 -0500

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -142,12 +142,12 @@ document.querySelectorAll('.btn').forEach(button => {
 });
 // Reviews Slider
 const reviews = [
-  { img: "/temp/pictures/pic1.png" },
-  { img: "/temp/pictures/pic12.png" },
-  { img: "/temp/pictures/pic2.png" },
-  { img: "/temp/pictures/pic22.png" },
-  { img: "/temp/pictures/pic3.png" },
-  { img: "/temp/pictures/pic32.png" }
+  { img: "/pictures/pic1.png" },
+  { img: "/pictures/pic12.png" },
+  { img: "/pictures/pic2.png" },
+  { img: "/pictures/pic22.png" },
+  { img: "/pictures/pic3.png" },
+  { img: "/pictures/pic32.png" }
 ];
 
 const reviewsTrack = document.querySelector('.reviews-track');
